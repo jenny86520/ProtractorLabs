dfd29ddf625d62e8f2cd9a68655423375daae81d
呂芯萍 <aka_lu@bankpro.com.tw>
2019-11-16 14:32:06 +0800

    start E2E test: add some note

--- protractor.conf.js
+++ protractor.conf.js
@@ -8,15 +8,20 @@ const { SpecReporter } = require('jasmine-spec-reporter');
  * @type { import("protractor").Config }
  */
 exports.config = {
-  allScriptsTimeout: 600000,
+  // 限制所有測試執行時間總長
+  allScriptsTimeout: 600000,  
+  // 設定測試檔檔名
   specs: [
     './src/**/*.e2e-spec.ts'
   ],
+  // 設定關閉 Control flow
+  SELENIUM_PROMISE_MANAGER: false,
+  // 設定瀏覽器
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
       args: [
-        // "--headless",
+        // "--headless",  // 若開啟，則將瀏覽器於背景打開
         // "--incognito",
         // "--start-maximized"
       ]
@@ -24,34 +29,42 @@ exports.config = {
   },
 
   // SELENIUM_PROMISE_MANAGER: false,
-
+  // 直接執行瀏覽器
   directConnect: true,
+  // 由 seleniumServer 待執行瀏覽器
   //seleniumServerJar: '../node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-3.141.59.jar',
+  // 設定根路徑
   baseUrl: 'http://localhost:4200/',
-
+  // 選擇使用 jasmine 測試框架
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 100000000,
+    defaultTimeoutInterval: 100000000,    // 限制單一測試執行時間總長
     print: function() {}
   },
-
+  // 前置設定
   async onPrepare() {
+    // 設定 ts 位置
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
-
+    // 設定 Report
     jasmine.getEnv().addReporter(new SpecReporter({
       // https://github.com/bcaudan/jasmine-spec-reporter/blob/master/src/configuration.ts
       spec: {
+        // 是否顯示全部錯誤訊息(true: 開啟 | false: 關閉)
         displayStacktrace: false
       }
     }));
 
     /**
+     * 使用保哥的 snippets
      * @type { import("protractor").ProtractorBrowser }
      */
-    // const browser = global['browser'];
+    const browser = global['browser'];
     // await browser.manage().timeouts().implicitlyWait(2000);
+
+    // 當專案不為 Angular 框架開發
+    // await browser.waitForAngularEnabled(false);
   }
 };
--- src/app.e2e-spec.ts
+++ src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { browser, logging, $, $$, by, element, ExpectedConditions as EC } from 'protractor';
-
+// 測試情境(名稱, callback function)
 describe('App', () => {
 
   const url = 'http://www.protractortest.org';
@@ -7,10 +7,11 @@ describe('App', () => {
   beforeEach(async () => {
     await browser.waitForAngularEnabled(true);
   });
-
+  // 測試案例(名稱, callback function)
   it('should show title', async () => {
     await browser.get(url);
     const title = await browser.getTitle();
+    // 預期結果
     expect(title).toContain('Protractor - end-to-end testing for AngularJS');
   });
 
