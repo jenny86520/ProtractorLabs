c054801e51a8a470bef1a8c517d0cf388f5a5b1c
呂芯萍 <aka_lu@bankpro.com.tw>
2019-11-30 10:29:57 +0800

    Class-1116-HW Review

--- src/events/create-event.e2e-spec.ts
+++ src/events/create-event.e2e-spec.ts
@@ -15,22 +15,8 @@ describe('Class-1116-HW: add a new event', () => {
         // 輸入活動名稱: Protractor 實戰
         await element(by.name('name')).sendKeys('Protractor 實戰');
         // 使用 Datepicker 選擇活動日期: 2019/11/16
-        await element(by.tagName('mat-datepicker-toggle')).click();
-        await browser.sleep(1000); // 等待1秒
-
-        const calendar = element(by.tagName('mat-calendar'));
-        await calendar.element(by.className('mat-calendar-header')).element(by.className('mat-calendar-period-button mat-button')).click();
-        await browser.sleep(1000); // 等待1秒
-
-        const calendar_body = calendar.element(by.tagName('tbody'));
-        await calendar_body.element(by.cssContainingText('div', '2019')).click();
-        await browser.sleep(1000); // 等待1秒
-
-        await calendar_body.element(by.cssContainingText('div', 'NOV')).click();
-        await browser.sleep(1000); // 等待1秒
-
-        await calendar_body.element(by.cssContainingText('div', '16')).click();
-        await browser.sleep(1000); // 等待1秒
+        // TODO: 進階練習 ：請嘗試設定 1997/12/31 這個日期！
+        await selectDate();
         // 輸入活動時間: 早上
         await element(by.name('time')).sendKeys('早上');
         // 輸入活動票價: 500
@@ -41,7 +27,7 @@ describe('Class-1116-HW: add a new event', () => {
         await element(by.name('country')).sendKeys('台灣');
         // 輸入活動網址: http://example.com
         await element(by.name('onlineUrl')).sendKeys('http://example.com');
-        // TODO: 上傳活動圖片: e2e/src/assets/Protractor.png
+        // 上傳活動圖片: e2e/src/assets/Protractor.png
         const imgPath = path.resolve('./src/assets/Protractor.png');
         await element(by.name('imageFile')).sendKeys(imgPath);
         // 點擊 儲存 按鈕
@@ -53,4 +39,21 @@ describe('Class-1116-HW: add a new event', () => {
         expect(events).toContain('PROTRACTOR 實戰');
     });
 });
+// 使用 Datepicker 選擇活動日期: 2019/11/16
+// 建議包成方法，不須撰寫重複的程式(if 很多地方使用到日期選擇器)
+// 如為別人實作的日期選擇器，不需替別人作測試!
+async function selectDate() {
+    await element(by.tagName('mat-datepicker-toggle')).click();
+    await browser.sleep(1000); // 等待1秒
+    const calendar = element(by.tagName('mat-calendar'));
+    await calendar.element(by.className('mat-calendar-header')).element(by.className('mat-calendar-period-button mat-button')).click();
+    await browser.sleep(1000); // 等待1秒
+    const calendar_body = calendar.element(by.tagName('tbody'));
+    await calendar_body.element(by.cssContainingText('div', '2019')).click();
+    await browser.sleep(1000); // 等待1秒
+    await calendar_body.element(by.cssContainingText('div', 'NOV')).click();
+    await browser.sleep(1000); // 等待1秒
+    await calendar_body.element(by.cssContainingText('div', '16')).click();
+    await browser.sleep(1000); // 等待1秒
+}
 /** [end] 實戰演練：練習表單操作 (回家作業) */
